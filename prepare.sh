630ce6e01f89f41d5895e9fc91d4457fa74b59af
Eli Ben-Shoshan <ebs@ufl.edu>
2023-10-25 16:35:46 -0400

    moved date code lower in script

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-thisMonth=$(date +%Y-%m)
-monthName=$(date +%B)
-
 if [ $# -lt 1 ]; then
     echo "Usage: $0 <filename>"
     exit 1
 fi
-    
+
+thisMonth=$(date +%Y-%m)
+monthName=$(date +%B)
+rm -f ${monthName}.csv
+
 # get the header from the CSV file
 head -n1 $1 > ${monthName}.csv
 
