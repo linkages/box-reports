af8d7879f8ca2483da938dcb8ca46f7ea256090d
Eli Ben-Shoshan <ebs@ufl.edu>
2023-10-25 16:44:39 -0400

    removed reportGB and reportTB functions

--- functions.py
+++ functions.py
@@ -1,17 +1,3 @@
-
-def reportGB(usage):
-    for user in usage:
-        asize = usage[user] / 1024 / 1024
-        roundedSize = round(asize, 3)
-        print(f'{user}: {roundedSize} GB')
-
-
-def reportTB(usage):
-    for user in usage:
-        asize = usage[user] / 1024 / 1024 / 1024
-        roundedSize = round(asize, 3)
-        print(f'{user}: {roundedSize} TB')
-
 def reportTotals(usage):
     for action in usage:
         print(f"{action}: ")
